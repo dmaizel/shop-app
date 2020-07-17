72923fad113114ea3f7bf97b4ea1efdbd8de17c3
daniel <dani98000k@gmail.com>
2020-07-17 13:23:27 +0300

    first commit

---
+++ components/shop/ProductItem.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableNativeFeedback,
+  Platform,
+} from 'react-native';
+
+import Card from '../UI/Card';
+
+const ProductItem = (props) => {
+  let TouchableCmp = TouchableOpacity;
+
+  if (Platform.OS === 'android' && Platform.Version >= 21) {
+    TouchableCmp = TouchableNativeFeedback;
+  }
+
+  return (
+    <Card style={styles.product}>
+      <TouchableCmp onPress={props.onSelect} useForeground>
+        <View>
+          <View style={styles.imageContainer}>
+            <Image style={styles.image} source={{ uri: props.image }} />
+          </View>
+          <View style={styles.details}>
+            <Text style={styles.title}>{props.title}</Text>
+            <Text style={styles.price}>${props.price.toFixed(2)}</Text>
+          </View>
+          <View style={styles.actions}>{props.children}</View>
+        </View>
+      </TouchableCmp>
+    </Card>
+  );
+};
+
+const styles = StyleSheet.create({
+  product: {
+    height: 300,
+    margin: 20,
+    overflow: 'hidden',
+  },
+  imageContainer: {
+    width: '100%',
+    height: '60%',
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    overflow: 'hidden',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+  title: {
+    fontSize: 18,
+    marginVertical: 2,
+    fontFamily: 'open-sans-bold',
+  },
+  price: {
+    fontSize: 14,
+    color: '#888',
+    fontFamily: 'open-sans',
+  },
+  actions: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: '23%',
+    paddingHorizontal: 20,
+  },
+  details: {
+    alignItems: 'center',
+    height: '17%',
+    padding: 10,
+  },
+});
+
+export default ProductItem;
